5c69282c9dac7523adb8810bbb72e92973341ffa
RenanSui <renanddtao@gmail.com>
2024-04-19 11:47:16 -0300

    remove users route, update index view

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+const createError = require('http-errors');
+const express = require('express');
+const expressLayout = require('express-ejs-layouts')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const aboutRouter = require('./routes/about');
 
-const PORT = 4000
-var app = express();
+const app = express();
+const PORT = 4000 || process.env.PORT
 
 // view engine setup
+app.use(expressLayout)
+app.set('layout', './layouts/main')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -21,15 +25,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -40,6 +44,9 @@ app.use(function(err, req, res, next) {
 });
 
 // listener
-app.listen(PORT, () => console.log('listening to port: ' + PORT))
+app.listen(PORT, () => {
+  console.log('listening to port: ' + PORT)
+  console.log('http://localhost:' + PORT)
+})
 
 module.exports = app;
