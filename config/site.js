fd0b95bacda8234207e8a2d730832b3950fadb04
RenanSui <renanddtao@gmail.com>
2024-04-19 13:41:53 -0300

    add links to site config

--- config/site.js
+++ config/site.js
@@ -1,6 +1,11 @@
+const links = {
+  githubAccount: 'https://github.com/RenanSui',
+}
+
 const siteConfig = {
   title: 'NodeJS Blog',
-  description: 'Simple blog created with NodeJS, Express & Mongodb'
+  description: 'Simple blog created with NodeJS, Express & Mongodb',
+  links
 }
 
 module.exports = siteConfig;
