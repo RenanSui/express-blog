02cc679f4b8e25b7b5092c48f42c47152a0f5331
RenanSui <renanddtao@gmail.com>
2024-04-19 17:00:40 -0300

    add posts from mongodb database

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const siteConfig = require('../config/site')
+const Post = require('../database/models/Post')
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { ...siteConfig });
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.find()
+    console.log({ posts })
+    res.render('index', { ...siteConfig, posts });
+  } catch (error) {
+    next(createError(404));
+  }
 });
+
+function insertPostData() {
+  Post.insertMany([
+    {
+      title: 'Building a Blog',
+      body: "This is the body text"
+    }
+  ])
+}
 
 module.exports = router;
