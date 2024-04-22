24f018ec622313093a3938b33dcdcf0250bebfef
RenanSui <renanddtao@gmail.com>
2024-04-22 17:14:57 -0300

    dynamic navbar

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,8 +2,22 @@ const links = {
   githubAccount: 'https://github.com/RenanSui',
 }
 
+const navbar = {
+  items: [
+    {
+      title: 'Home',
+      href: '/',
+    },
+    {
+      title: 'Admin',
+      href: '/admin',
+    },
+  ],
+}
+
 export const siteConfig = {
   title: 'Sui Blog',
   description: 'Simple blog created with NodeJS, Express & Mongodb',
   links,
+  mainNav: [...navbar.items],
 }
