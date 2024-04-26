5036cef93f830e0c57327b39666b7e60b96ade87
RenanSui <renanddtao@gmail.com>
2024-04-26 04:20:32 -0300

    update authValidation

--- src/validations/authValidation.ts
+++ src/validations/authValidation.ts
@@ -1,46 +1,47 @@
-import { SignUpPayload } from '@/types/auth'
+import { SignInPayload } from '@/types/auth'
 import { BodyRequest } from '@/types/request'
 import { NextFunction, Response } from 'express'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
-import ValidatorJS from 'validatorjs'
-
-const validateRule = {
-  email: 'required|email',
-  password: 'required|min:8|max:16',
-}
+import validator from 'validator'
 
 export const authValidation = {
-  signIn(req: BodyRequest<SignUpPayload>, res: Response, next: NextFunction) {
-    const { email, password } = req.body
+  signIn: (
+    req: BodyRequest<SignInPayload>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      if (!req.body.email || !req.body.password) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: ReasonPhrases.BAD_REQUEST,
+          status: StatusCodes.BAD_REQUEST,
+        })
+      }
 
-    const validation = new ValidatorJS({ email, password }, validateRule)
+      let normalizedEmail =
+        req.body.email && validator.normalizeEmail(req.body.email)
+      if (normalizedEmail) {
+        normalizedEmail = validator.trim(normalizedEmail)
+      }
 
-    validation.fails(() => {
+      if (
+        !normalizedEmail ||
+        !validator.isEmail(normalizedEmail, { allow_utf8_local_part: false })
+      ) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: ReasonPhrases.BAD_REQUEST,
+          status: StatusCodes.BAD_REQUEST,
+        })
+      }
+
+      Object.assign(req.body, { email: normalizedEmail })
+
+      return next()
+    } catch (error) {
       return res.status(StatusCodes.BAD_REQUEST).json({
         message: ReasonPhrases.BAD_REQUEST,
         status: StatusCodes.BAD_REQUEST,
       })
-    })
-
-    validation.passes(() => {
-      return next()
-    })
-  },
-
-  signUp(req: BodyRequest<SignUpPayload>, res: Response, next: NextFunction) {
-    const { email, password } = req.body
-
-    const validation = new ValidatorJS({ email, password }, validateRule)
-
-    validation.fails(() => {
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        message: ReasonPhrases.BAD_REQUEST,
-        status: StatusCodes.BAD_REQUEST,
-      })
-    })
-
-    validation.passes(() => {
-      return next()
-    })
+    }
   },
 }
