143b8ad0cf328840516290dc58f14e6a3c20df38
RenanSui <renanddtao@gmail.com>
2024-04-26 04:23:54 -0300

    update authController signIn to handle errors and send accesstoken

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,23 +1,63 @@
 import { createHash } from '@/lib/hash'
 import { userService } from '@/service/userService'
+import { SignInPayload, SignUpPayload } from '@/types/auth'
+import { BodyRequest } from '@/types/request'
 import { jwtSign } from '@/utils/jwt'
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import { startSession } from 'mongoose'
 
+// userService.login({ email: req.body.email, password: req.body.password })
+// res.redirect('/')
+
+// // can only be accessed by server requests
+// // path = where the cookie is valid
+// path: '/',
+// // domain = what domain the cookie is valid on
+// domain: 'localhost',
+// // secure = only send cookie over https
+// secure: false,
+// // sameSite = only send cookie if the request is coming from the same origin
+// sameSite: 'lax', // "strict" | "lax" | "none" (secure must be true)
+// // maxAge = how long the cookie is valid for in milliseconds
+// .setHeader('Access-Control-Allow-Credentials', 'true')
+
 export const authController = {
-  signIn: (req: Request, res: Response) => {
-    // userService.login({ email: req.body.email, password: req.body.password })
-    res.redirect('/')
+  signIn: async (
+    { body: { email, password } }: BodyRequest<SignInPayload>,
+    res: Response,
+  ) => {
+    try {
+      const user = await userService.getByEmail(email)
+
+      const comparePassword = user?.comparePassword(password)
+      if (!user || !comparePassword) {
+        return res.status(StatusCodes.NOT_FOUND).json({
+          message: ReasonPhrases.NOT_FOUND,
+          status: StatusCodes.NOT_FOUND,
+        })
+      }
+
+      const { accessToken } = jwtSign(user.id)
+
+      return res.status(StatusCodes.OK).json({
+        data: { accessToken },
+        message: ReasonPhrases.OK,
+        status: StatusCodes.OK,
+      })
+    } catch (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: ReasonPhrases.BAD_REQUEST,
+        status: StatusCodes.BAD_REQUEST,
+      })
+    }
   },
 
-  // { body: { email, password } }: BodyRequest<SignUpPayload>,
-  signUp: async (req: Request, res: Response, next: NextFunction) => {
+  signUp: async (
+    { body: { email, password } }: BodyRequest<SignUpPayload>,
+    res: Response,
+  ) => {
     const session = await startSession()
-    const {
-      body: { email, password },
-    } = req
-
     try {
       const isUserExist = await userService.isExistByEmail(email)
 
@@ -29,7 +69,6 @@ export const authController = {
       }
 
       session.startTransaction()
-
       const hashedPassword = await createHash(password)
 
       const user = await userService.create(
@@ -45,13 +84,21 @@ export const authController = {
       await session.commitTransaction()
       session.endSession()
 
-      res.cookie('jwt', accessToken, {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24, // 1 day
+      return res.status(StatusCodes.OK).json({
+        data: { accessToken },
+        message: ReasonPhrases.OK,
+        status: StatusCodes.OK,
       })
-      return next()
     } catch (error) {
-      return next()
+      if (session.inTransaction()) {
+        await session.abortTransaction()
+        session.endSession()
+      }
+
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: ReasonPhrases.BAD_REQUEST,
+        status: StatusCodes.BAD_REQUEST,
+      })
     }
   },
 
