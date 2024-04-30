cd7c403de29098e07658c3a14f61a10bb23e0f7c
RenanSui <renanddtao@gmail.com>
2024-04-30 13:16:42 -0300

    add more request types

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,5 +1,16 @@
 import { Request } from 'express'
+import { Document } from 'mongoose'
+import { User } from '.'
 
 export interface BodyRequest<T> extends Omit<Request, 'body'> {
   body: T
+}
+
+export interface ContextRequest<T> extends Omit<Request, 'context'> {
+  context: T
+}
+
+export interface UserRequest {
+  user: Omit<User, 'id'> & Document
+  accessToken: string
 }
