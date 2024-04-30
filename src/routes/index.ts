526f2d58cf057884e95a2cd49e5400dda9f6d370
RenanSui <renanddtao@gmail.com>
2024-04-30 13:55:36 -0300

    add user to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { auth } from './auth'
 import { blog } from './blog'
+import { user } from './user'
 
 const router: Router = Router()
 
 const routes: { [key: string]: (router: Router) => void } = {
   auth,
   blog,
+  user,
 }
 
 for (const route in routes) {
