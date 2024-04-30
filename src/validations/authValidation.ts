c2388467e464b5a02cda37eaafa4f853513b02b9
RenanSui <renanddtao@gmail.com>
2024-04-30 11:54:19 -0300

    add signup auth validation

--- src/validations/authValidation.ts
+++ src/validations/authValidation.ts
@@ -49,5 +49,43 @@ export const authValidation = {
     req: BodyRequest<SignUpPayload>,
     res: Response,
     next: NextFunction,
-  ) => next(),
+  ) => {
+    try {
+      if (
+        !req.body.email ||
+        !req.body.password ||
+        !validator.isLength(req.body.password, { min: 6, max: 48 })
+      ) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: ReasonPhrases.BAD_REQUEST,
+          status: StatusCodes.BAD_REQUEST,
+        })
+      }
+
+      let normalizedEmail =
+        req.body.email && validator.normalizeEmail(req.body.email)
+      if (normalizedEmail) {
+        normalizedEmail = validator.trim(normalizedEmail)
+      }
+
+      if (
+        !normalizedEmail ||
+        !validator.isEmail(normalizedEmail, { allow_utf8_local_part: false })
+      ) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: ReasonPhrases.BAD_REQUEST,
+          status: StatusCodes.BAD_REQUEST,
+        })
+      }
+
+      Object.assign(req.body, { email: normalizedEmail })
+
+      return next()
+    } catch (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: ReasonPhrases.BAD_REQUEST,
+        status: StatusCodes.BAD_REQUEST,
+      })
+    }
+  },
 }
