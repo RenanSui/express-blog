2a9b4c34f0f1f3832446f8b7e9052719a864c269
RenanSui <renanddtao@gmail.com>
2024-05-07 17:30:37 -0300

    add blog post create route

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,4 +1,6 @@
 import { blogController } from '@/controllers/blogController'
+import { authGuard } from '@/guards'
+import { blogValidation } from '@/validations/blogValidation'
 import { Router } from 'express'
 
 export const blog = (router: Router): void => {
@@ -7,4 +9,11 @@ export const blog = (router: Router): void => {
   router.get('/blog/post/id/:id', blogController.postId)
 
   router.post('/blog/post/search', blogController.postSearch)
+
+  router.post(
+    '/blog/post/create',
+    authGuard.isAuth,
+    blogValidation.createPost,
+    blogController.createPost,
+  )
 }
