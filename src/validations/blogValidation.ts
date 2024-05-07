52c372bd8b989a9d273a7c8f9470aceb7b8f22a2
RenanSui <renanddtao@gmail.com>
2024-05-07 17:26:02 -0300

    create blog validation

---
+++ src/validations/blogValidation.ts
@@ -0,0 +1,30 @@
+import { CreatePostPayload } from '@/types/post'
+import { BodyRequest } from '@/types/request'
+import { NextFunction, Response } from 'express'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
+
+export const blogValidation = {
+  createPost: (
+    req: BodyRequest<CreatePostPayload>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      if (!req.body.title || !req.body.body) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: ReasonPhrases.BAD_REQUEST,
+          status: StatusCodes.BAD_REQUEST,
+        })
+      }
+
+      Object.assign(req.body, { ...req.body })
+
+      return next()
+    } catch (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: ReasonPhrases.BAD_REQUEST,
+        status: StatusCodes.BAD_REQUEST,
+      })
+    }
+  },
+}
