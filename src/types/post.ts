16ba27d3b9bb6f54f05f91629f1d2ba6c1187a44
RenanSui <renanddtao@gmail.com>
2024-05-07 17:28:39 -0300

    extend post types

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,4 +1,13 @@
-import { Post } from '.'
+import { ObjectId } from 'mongoose'
+
+export type Post = {
+  id: ObjectId
+  userId: string
+  title: string
+  body: string
+  createdAt: Date
+  updatedAt: Date
+}
 
 export type CreatePostProps = Pick<Post, 'title' | 'body' | 'userId'>
 
