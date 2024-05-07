c9981f2976876124bdc8a5a97ea7b01833da957c
RenanSui <renanddtao@gmail.com>
2024-05-07 17:26:49 -0300

    update post model

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,11 +1,13 @@
 import { Post } from '@/types'
 import { Schema, model } from 'mongoose'
 
-const schema = new Schema<Post>({
-  title: String,
-  body: String,
-  createdAt: Date,
-  updatedAt: Date,
-})
+const schema = new Schema<Post>(
+  {
+    title: String,
+    body: String,
+    userId: String,
+  },
+  { timestamps: true },
+)
 
 export const PostModel = model('Post', schema)
