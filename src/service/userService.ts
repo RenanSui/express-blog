d03f67a8103197b0c9136a7c5432211b5de3e388
RenanSui <renanddtao@gmail.com>
2024-05-16 16:45:01 -0300

    extend user service

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,6 +1,6 @@
-import { generateFromEmail, generateUsername } from 'unique-username-generator'
-import { ClientSession, ObjectId } from 'mongoose'
 import { UserModel } from '@/models'
+import { ClientSession, ObjectId } from 'mongoose'
+import { generateFromEmail, generateUsername } from 'unique-username-generator'
 
 export const userService = {
   create: async (
@@ -13,6 +13,24 @@ export const userService = {
       email,
       password,
     }).save({ session }),
+
+  updateProfileByUserId: (
+    userId: ObjectId,
+    { name, username }: { name: string; username: string },
+    session?: ClientSession,
+  ) => {
+    const data = [{ _id: userId }, { name, username }]
+
+    let params = null
+
+    if (session) {
+      params = [...data, { session }]
+    } else {
+      params = data
+    }
+
+    return UserModel.updateOne(...params)
+  },
 
   getById: (userId: ObjectId) => UserModel.findById(userId),
 
