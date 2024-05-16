1e202ab601e0c62f0010d85a8e25eead6f87655e
RenanSui <renanddtao@gmail.com>
2024-05-16 16:45:50 -0300

    create user update route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,15 @@
 import { userController } from '@/controllers/userController'
 import { authGuard } from '@/guards'
+import { userValidation } from '@/validations/userValidation'
 import { Router } from 'express'
 
 export const user = (router: Router): void => {
   router.get('/me', authGuard.isAuth, userController.me)
+
+  router.post(
+    '/user/update',
+    authGuard.isAuth,
+    userValidation.updateProfile,
+    userController.updateProfile,
+  )
 }
