21e844d0116e76b2e2762804bf388e8293ffbf84
RenanSui <renanddtao@gmail.com>
2024-05-16 16:42:43 -0300

    create user types

---
+++ src/types/user.ts
@@ -0,0 +1,18 @@
+import { Model } from 'mongoose'
+
+export interface IUserMethods {
+  comparePassword: (password: string) => boolean
+}
+
+export type User = {
+  name: string
+  email: string
+  password: string
+  username: string
+  imageUrl: string
+  isAdmin: boolean
+}
+
+export type IUserModel = Model<User, unknown, IUserMethods>
+
+export type UpdateProfilePayload = Required<Pick<User, 'name' | 'username'>>
