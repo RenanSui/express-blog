ccd73112787ff5323b38673ce388e8fe7b36800d
RenanSui <renanddtao@gmail.com>
2024-05-16 16:44:32 -0300

    create userValidation

---
+++ src/validations/userValidation.ts
@@ -0,0 +1,47 @@
+import { BodyRequest } from '@/types/request'
+import { UpdateProfilePayload } from '@/types/user'
+import { NextFunction, Response } from 'express'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
+import validator from 'validator'
+
+export const userValidation = {
+  updateProfile: (
+    req: BodyRequest<UpdateProfilePayload>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      if (!req.body.name || !req.body.username) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: ReasonPhrases.BAD_REQUEST,
+          status: StatusCodes.BAD_REQUEST,
+        })
+      }
+
+      const trimemdName = validator.trim(req.body.name)
+      const trimemdUsername = validator.trim(req.body.username)
+
+      if (
+        !validator.isLength(trimemdName, { min: 2, max: 48 }) ||
+        !validator.isLength(trimemdUsername, { min: 2, max: 48 })
+      ) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: ReasonPhrases.BAD_REQUEST,
+          status: StatusCodes.BAD_REQUEST,
+        })
+      }
+
+      Object.assign(req.body, {
+        name: trimemdName,
+        username: trimemdUsername,
+      })
+
+      return next()
+    } catch (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: ReasonPhrases.BAD_REQUEST,
+        status: StatusCodes.BAD_REQUEST,
+      })
+    }
+  },
+}
