68416f0f95d94888560b32beb60886129b3227a5
RenanSui <renanddtao@gmail.com>
2024-05-16 16:43:40 -0300

    extend request types

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
 import { Document } from 'mongoose'
-import { User } from '.'
+import { User } from './user'
 
 export interface BodyRequest<T> extends Omit<Request, 'body'> {
   body: T
@@ -19,3 +20,21 @@ export type BodyContextRequest<T, B> = Omit<Request, 'body' | 'context'> & {
   body: T
   context: B
 }
+
+export interface IParamsRequest<T> extends Request {
+  params: T & ParamsDictionary
+}
+
+export interface IQueryRequest<T> extends Request {
+  query: T & ParamsDictionary
+}
+
+export interface ICombinedRequest<
+  Context,
+  Body,
+  Params = Record<string, unknown>,
+  Query = Record<string, unknown>,
+> extends Pick<ContextRequest<Context>, 'context'>,
+    Pick<BodyRequest<Body>, 'body'>,
+    Pick<IParamsRequest<Params>, 'params'>,
+    Pick<IQueryRequest<Query>, 'query'> {}
